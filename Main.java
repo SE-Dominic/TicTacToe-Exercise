808f4d561cb8e62136fa7fa8c391826f327ddb5c
Dom.SEngineer <dominic.allen998@gmail.com>
2025-02-04 09:45:43 -0800

    new messagfe

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
-class Main {
+class TicTacToe {
+  int BOARDSIZE = 3;
+  char board[][];
+  boolean firstPlayer;
+  boolean secondPlayer;
   public static void main(String[] args) {
        System.out.println("Hello world");
     }
