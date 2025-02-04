8951a3b5c77d31dbda0e9fe5df8f84832bb5ba0f
Dom.SEngineer <dominic.allen998@gmail.com>
2025-02-04 09:54:27 -0800

    added function definitions

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
+enum Status {
+  WIN, DRAW, CONTINUE
+}
 class TicTacToe {
-  int BOARDSIZE = 3;
-  char board[][];
-  boolean firstPlayer;
-  boolean secondPlayer;
+  private int BOARDSIZE = 3;
+  private char board[][];
+  private boolean firstPlayer;
+  private boolean secondPlayer;
+  private Status status;
+
   public static void main(String[] args) {
        System.out.println("Hello world");
+    }
+
+    public static void play() {
+
+    }
+    public static void printBoard(){
+
+    }
+    private static void printStatus(){
+
+    }
+    private static boolean validMove(){
+      return false;}
+
+    private static char printSymbol() {
+      return 'a';
     }
 }
