5e78ab43796c87f3ce567237e42c6834b4cde57b
lambo_n <nathanyflam@gmail.com>
2025-02-04 10:00:18 -0800

    fix formatting

--- Main.java
+++ Main.java
@@ -1,30 +1,31 @@
 enum Status {
   WIN, DRAW, CONTINUE
 }
+
 class TicTacToe {
   private int BOARDSIZE = 3;
-  private char board[][];
+  private char board[][] = new char[BOARDSIZE][BOARDSIZE];
   private boolean firstPlayer;
   private boolean secondPlayer;
   private Status status;
 
   public static void main(String[] args) {
        System.out.println("Hello world");
-    }
+  }
 
-    public static void play() {
+  public static void play() {
 
-    }
-    public static void printBoard(){
+  }
+  public static void printBoard(){
 
-    }
-    private static void printStatus(){
+  }
+  private static void printStatus(){
 
-    }
-    private static boolean validMove(){
-      return false;}
+  }
+  private static boolean validMove(){
+    return false;}
 
-    private static char printSymbol() {
-      return 'a';
-    }
+  private static char printSymbol() {
+    return 'a';
+  }
 }
